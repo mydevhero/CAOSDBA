0896d3547a6b341b065d4caa5f708f854a632ec0
mydevhero <mydevhero@gmail.com>
2025-11-16 16:43:48 +0100

    Fix issue on variable version

--- libcaos/cmake/build_counter_and_version.cmake
+++ libcaos/cmake/build_counter_and_version.cmake
@@ -1,5 +1,5 @@
-set(BUILD_COUNTER_FILE "${CMAKE_BINARY_DIR}/build_counter.txt")
-string(TIMESTAMP BUILD_TIMESTAMP "%Y-%m-%d %H:%M:%S")
+set(BUILD_COUNTER_FILE "${CAOS_BINARY_DIR}/build_counter.txt")
+string(TIMESTAMP BUILD_TIMESTAMP "%Y-%m-%d_%H:%M:%S")
 
 # Build counter
 if(EXISTS "${BUILD_COUNTER_FILE}")
@@ -12,24 +12,48 @@ endif()
 
 file(WRITE "${BUILD_COUNTER_FILE}" "${CAOS_BUILD_COUNT}")
 
-# Crea manualmente il file header
-set(BUILD_INFO_H_CONTENT "
-// File generato automaticamente - non modificare!
-#pragma once
+# Imposta le variabili che verranno sostituite nel template
+set(CAOS_BUILD_COUNT ${CAOS_BUILD_COUNT})
+set(BUILD_TIMESTAMP "${BUILD_TIMESTAMP}")
+set(PROJECT_NAME "${PROJECT_NAME}")
+set(PROJECT_VERSION "${PROJECT_VERSION}")
+set(PROJECT_VERSION_MAJOR "${PROJECT_VERSION_MAJOR}")
+set(PROJECT_VERSION_MINOR "${PROJECT_VERSION_MINOR}")
+set(PROJECT_VERSION_PATCH "${PROJECT_VERSION_PATCH}")
 
-#define CAOS_BUILD_COUNT ${CAOS_BUILD_COUNT}
-#define CAOS_BUILD_TIMESTAMP \"${BUILD_TIMESTAMP}\"
-#define CAOS_NAME \"@PROJECT_NAME@\"
-#define CAOS_VERSION \"@PROJECT_VERSION@\"
-#define CAOS_VERSION_MAJOR \"@PROJECT_VERSION_MAJOR@\"
-#define CAOS_VERSION_MINOR \"@PROJECT_VERSION_MINOR@\"
-#define CAOS_VERSION_PATCH \"@PROJECT_VERSION_PATCH@\"
-")
-
-# Scrivi il file header manualmente
-file(WRITE "${CMAKE_BINARY_DIR}/build_info.h" "${BUILD_INFO_H_CONTENT}")
+# Usa il percorso assoluto passato come variabile
+configure_file(
+    ${CAOS_SOURCE_DIR}/build_info.h.in
+    ${CAOS_BINARY_DIR}/libcaos/build_info.h
+    @ONLY
+)
 
 message(STATUS "Build counter incremented to: ${CAOS_BUILD_COUNT}")
+
+# set(BUILD_COUNTER_FILE "${CMAKE_BINARY_DIR}/build_counter.txt")
+# string(TIMESTAMP BUILD_TIMESTAMP "%Y-%m-%d %H:%M:%S")
+
+# # Build counter
+# if(EXISTS "${BUILD_COUNTER_FILE}")
+#     file(READ "${BUILD_COUNTER_FILE}" CAOS_BUILD_COUNT)
+#     string(STRIP "${CAOS_BUILD_COUNT}" CAOS_BUILD_COUNT)
+#     math(EXPR CAOS_BUILD_COUNT "${CAOS_BUILD_COUNT} + 1")
+# else()
+#     set(CAOS_BUILD_COUNT 1)
+# endif()
+
+# file(WRITE "${BUILD_COUNTER_FILE}" "${CAOS_BUILD_COUNT}")
+
+# # Imposta le variabili che verranno sostituite nel template
+# set(CAOS_BUILD_COUNT ${CAOS_BUILD_COUNT})
+# set(BUILD_TIMESTAMP "${BUILD_TIMESTAMP}")
+# set(PROJECT_NAME "${PROJECT_NAME}")
+# set(PROJECT_VERSION "${PROJECT_VERSION}")
+# set(PROJECT_VERSION_MAJOR "${PROJECT_VERSION_MAJOR}")
+# set(PROJECT_VERSION_MINOR "${PROJECT_VERSION_MINOR}")
+# set(PROJECT_VERSION_PATCH "${PROJECT_VERSION_PATCH}")
+
+# message(STATUS "Build counter incremented to: ${CAOS_BUILD_COUNT}")
 
 # Include generated directory
 # target_include_directories(${PROJECT_NAME} PUBLIC
